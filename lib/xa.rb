ae65dcf9533d33e82a824cf89f68afccbeef8503
OKURA Masafumi <masafumi.o1988@gmail.com>
2022-02-11 23:37:19 +0900

    Initial commit

---
+++ lib/xa.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "xa/version"
+
+module Xa
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/xa/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Xa
+  VERSION = "0.1.0"
+end
