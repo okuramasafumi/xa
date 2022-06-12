05fb27259730a2b04fea5d876a7ec523002471cc
OKURA Masafumi <masafumi.o1988@gmail.com>
2022-06-12 19:09:19 +0900

    Use `require_relative` to require test_helper
    It makes it possible to run test individually.
    It also avoid errors from typeprof.

--- test/test_initialize.rb
+++ test/test_initialize.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "test_helper"
 
 class TestInitialize < Minitest::Test
   def test_it_does_not_raise_exception_when_required_methods_are_implemented
